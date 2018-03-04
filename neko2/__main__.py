afafbfe4aab4917bbfdf50d3598e7640c5de8216
Esp <deltamimikyu@outlook.com>
2018-03-04 14:30:31 +0000

    More fixing

--- neko2/__main__.py
+++ neko2/__main__.py
@@ -3,6 +3,8 @@
 """
 Application entry point.
 """
+import traceback
+
 from neko2.engine import autoloader
 from neko2.engine import client
 from neko2.shared import configfiles
@@ -11,7 +13,18 @@ from neko2.shared import configfiles
 cfg_file = configfiles.get_config_holder('discord.yaml')
 
 bot = client.Bot(cfg_file.sync_get())
-autoloader.auto_load_modules(bot)
+errors = autoloader.auto_load_modules(bot)
+
+@bot.listen()
+async def on_ready():
+    if errors:
+        for error in errors:
+            string = ''.join(traceback.format_exception(
+                type(error),
+                error,
+                error.__traceback__)
+            await bot.get_user(bot.owner_id).send(string[:2000])
+        errors = None
 
 bot.run()
 
--- neko2/engine/autoloader.py
+++ neko2/engine/autoloader.py
@@ -26,14 +26,15 @@ def auto_load_modules(bot, *, config_file=FILE):
             'modules.yaml' file if unspecified.
 
     If any extensions fail to load, then we do not halt. A traceback is printed
-    and we continue.
+    and we continue. Any errors are returned in a collection.
     """
     config = configfiles.get_config_data(config_file)
     logger = logging.getLogger(__name__)
 
+    errors = []
+
     if config is None:
         logger.warning('No modules were listed.')
-        return
     elif not isinstance(config, list):
         raise TypeError('Expected list of module names.')
     else:
@@ -41,8 +42,10 @@ def auto_load_modules(bot, *, config_file=FILE):
             # noinspection PyBroadException
             try:
                 _, time = alg.time_it(lambda: bot.load_extension(module))
-            except BaseException:
+            except BaseException as ex:
                 logger.fatal(f'FAILED TO LOAD {module}. SEE TRACEBACK BELOW.')
                 traceback.print_exc()
+                errors.append(ex)
             else:
                 logger.info(f'Loaded module {module} in {time * 1000:,.2f}ms')
+    return errors
