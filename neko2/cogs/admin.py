fb39db43139ea0174cd4d992bfa51915e279ccd6
404 <deltamimikyu@outlook.com>
2018-03-17 10:06:02 +0000

    Added ping command.

--- neko2/cogs/admin.py
+++ neko2/cogs/admin.py
@@ -4,6 +4,8 @@
 Cog holding owner-only administrative commands, such as those for restarting
 the bot, inspecting/loading/unloading commands/cogs/extensions, etc.
 """
+import asyncio
+
 from neko2.engine import commands   # command decorator
 
 
@@ -19,13 +21,16 @@ class AdminCog:
         Kills the bot. If it is running as a systemd service, this should
         cause the bot to restart.
         """
-        try:
-            await ctx.add_reaction('\N{OK HAND SIGN}')
-        except:
-            await ctx.send('\N{OK HAND SIGN}')
-        finally:
-            await ctx.bot.logout()
+        commands.acknowledge(ctx)
+        await asyncio.sleep(2)
+        await ctx.bot.logout()
 
+    @commands.command()
+    async def ping(self, ctx):
+        """
+        Checks whether the bot is online and responsive or not.
+        """
+        await ctx.send(f'Pong! ({ctx.bot.latency * 1000:.2f}ms)')
 
 
 def setup(bot):
