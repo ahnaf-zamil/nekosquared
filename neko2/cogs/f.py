5f52d5de44eec47b1246dc7c8585319826645c36
Espy <deltamimikyu@outlook.com>
2018-04-08 08:07:49 +0100

    Press F (more stuff)

--- neko2/cogs/f.py
+++ neko2/cogs/f.py
@@ -4,20 +4,37 @@
 Press F to pay respects.
 """
 import discord
+from neko2.engine import commands
 
 
 class RespectsCog:
-    async def on_message(self, message):
+    async def on_message(self, message, **kwargs):
         try:
-            if message.content.lower() == 'f':
+            if message.content.lower() == 'f' or 'for_what' in kwargs:
                 await message.delete()
+
+                text = f'{message.author} paid their respects'
+                if kwargs["for_what"]:
+                    text += f' for {kwargs["for_what"]}.'
+                else:
+                    text += '.'
+
                 resp = await message.channel.send(embed=discord.Embed(
-                    title=f'{message.author} paid their respects.',
+                    title=text,
                     colour=0x551a8b
                 ))
-                await resp.add_reaction('\N{REGIONAL INDICATOR SYMBOL LETTER F}')
+                await resp.add_reaction(
+                    '\N{REGIONAL INDICATOR SYMBOL LETTER F}')
         except:
+            raise
             pass
+
+    @commands.command(brief='Pay your respects.')
+    async def f(self, ctx, *, what=None):
+        prefix = ctx.prefix + ctx.invoked_with
+        await self.on_message(
+            ctx.message,
+            for_what=ctx.message.clean_content[len(prefix):].lstrip())
 
 
 def setup(bot):
