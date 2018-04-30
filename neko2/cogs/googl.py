687efdac5e8f1eae79f8736b55115fe2a87fa5c0
Esp <nekozilla404notfound@gmail.com>
2018-04-30 08:06:31 +0100

    Google command

--- neko2/cogs/googl.py
+++ neko2/cogs/googl.py
@@ -22,16 +22,17 @@ from neko2.shared import traits
 config_file = 'urlshorten'
 
 
+# noinspection PyBroadException
 class UrlShortenerCog(traits.CogTraits):
     """Shortens URLS"""
-    def __init__(self):
-        self._key: str = configfiles.get_config_data(config_file)
+    _key: str = configfiles.get_config_data(config_file)
 
-    async def _shorten(self, url, bot):
-        conn = await self.acquire_http()
+    @classmethod
+    async def googl(cls, url):
+        conn = await cls.acquire_http()
 
         res = await conn.post('https://www.googleapis.com/urlshortener/v1/url',
-                              params={'key': self._key},
+                              params={'key': cls._key},
                               json={'longUrl': url},
                               headers={'content_type': 'application/json'})
 
@@ -46,7 +47,7 @@ class UrlShortenerCog(traits.CogTraits):
         You can pass a description to put with the link if you like.
         """
         try:
-            url = await self._shorten(url, ctx.bot)
+            url = await self.googl(url)
         except BaseException:
             traceback.print_exc()
             await ctx.send('There was an error handling this request.')
@@ -91,7 +92,40 @@ class UrlShortenerCog(traits.CogTraits):
             await ctx.message.delete()
 
         url = f'http://lmgtfy.com?{frag}'
-        url = await self._shorten(url, ctx.bot)
+        url = await self.googl(url)
+
+        await ctx.send(''.join((mention, f'<{url}>')))
+
+    @commands.command(
+        brief='Links to a Google Search for the given terms.',
+        usage='query [| @mention]',
+        examples=['how to buy lime', 'what is a discord.py? | @mention#1234'],
+        aliases=['googled'])
+    async def google(self, ctx, *, query):
+        """
+        Adds a shortlink to the results for a given Google Search.
+
+        Call `googled` to delete the initial message.
+
+        You can "pipe" the output to a given member. This will mention them
+        in the response.
+        """
+        if '|' in query:
+            query, _, mention = query.rpartition('|')
+
+            mention = mention.strip()
+
+            if not re.match(r'^<!?@\d+>$', mention):
+                mention = ''
+            else:
+                query, mention = query.rstrip(), mention + ': '
+        else:
+            mention = ''
+
+        frag = urllib.parse.urlencode({'q': query})
+
+        url = f'https://www.google.com/search?{frag}'
+        url = await self.googl(url)
 
         await ctx.send(''.join((mention, f'<{url}>')))
 
