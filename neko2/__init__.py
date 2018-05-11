bfef9a0dfff09a107f873c91d83c7bf6cbd58946
Esp <nekozilla404notfound@gmail.com>
2018-05-11 08:23:08 +0100

    Updated to MIT license, added license to all file preambles.

--- neko2/__init__.py
+++ neko2/__init__.py
@@ -6,6 +6,31 @@ the price of lime.
 
 If you want to be nosey, you can also [check out the source code on GitHub.](
 https://github.com/neko404notfound/nekosquared)
+
+===
+
+MIT License
+
+Copyright (c) 2018 Neko404NotFound
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 import neko2.shared
 import neko2.cogs
@@ -13,7 +38,7 @@ import neko2.engine
 
 
 __author__ = 'Neko404NotFound'
-__license__ = 'Mozilla Public License Version 2.0'
+__license__ = 'MIT License'
 __url__ = 'https://github.com/neko404notfound/nekosquared'
 __version__ = '1.10.0-BLEEDING'
 __repository__ = __url__
--- neko2/engine/__init__.py
+++ neko2/engine/__init__.py
@@ -4,5 +4,30 @@
 Bot engine code. This is for general internal bits and bobs pertaining to how
 the bot runs. Cogs and extensions may use some of these utilities, but they are
 primarily for the main Discord.py bot wrapper.
+
+===
+
+MIT License
+
+Copyright (c) 2018 Neko404NotFound
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 from .client import *
