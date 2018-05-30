bf3b00945f25d4211f0386aa5d355427816029e1
Espy | Neko | 404 <34942042+neko404notfound@users.noreply.github.com>
2018-05-30 09:00:42 +0100

    Create other.py

---
+++ neko2/cogs/other.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+"""
+Other random stuff I like.
+"""
+from neko2.shared import alg
+from neko2.shared import commands
+
+
+class MiscCog:
+    # Only work for Guilds that I am actually in.
+    @commands.check(lambda c: alg.find(lambda m: m.id == 351216213327609858, 
+                                       c.guild.members))
+    @commands.command(brief='Gets Esp\'s Nintendo Switch code.', aliases=['sw', 'switch'])
+    async def nintendoswitch(self, ctx):
+        await ctx.send('SW-5006-9390-0438')
+
+
+def setup(bot):
+    bot.add_cog(MiscCog())
